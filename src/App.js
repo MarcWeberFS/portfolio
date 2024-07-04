1e363147b03b6bd9a71dee44e1ecc2e82a140115
MarcWeberFS <marc.robin.weber@gmail.com>
2024-07-04 04:05:09 +0200

    Content to the pages

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Spotlight />
+    <div className="App font-sans">
       <header className="App-header bg-slate-950 text-white">
-
-          <div className="relative w-full h-screen">
+        <div className="background-container">
           <ParticleCanvas />
+          <div className="inverted-canvas">
+            <ParticleCanvas />
+          </div>
         </div>
-        <div className="relative w-full h-screen transform scale-x-[-1]">
-          <ParticleCanvas />
+        <Spotlight />
+        <div className="content flex max-w-5xl mr-auto ml-auto text-left">
+          <div className="w-4/5">
+            <span className="text-5xl font-bold">Marc Weber</span>
+            <br></br>
+            <span className="text-2xl"> Full Stack Software Engineer </span>
+
+          </div>
+          <div className="w-full">
+            About
+            <br></br>
+            work experience
+            <br></br>
+            Projects
+            <br></br>
+          </div>
+
         </div>
-
-
       </header>
     </div>
   );
