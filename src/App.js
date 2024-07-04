e5845e903d317c1d82870fd8fbc58fc0bf0a578c
MarcWeberFS <marc.robin.weber@gmail.com>
2024-07-04 04:21:24 +0200

    added cards

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ParticleCanvas from './component/ParticleCanvas';
 import Spotlight from './component/Spotlight';
 import './App.css';
-
+import Card from './component/Card';
 function App() {
+
+  const card1 = {
+    Date: "July 2020 - present",
+    Role: "Full Stack Software Engineer",
+    Description: "Test Test Test",
+    Skills: "Test Test Test"
+  };
+
+
   return (
     <div className="App font-sans">
       <header className="App-header bg-slate-950 text-white">
@@ -18,10 +27,16 @@ function App() {
           <div className="w-4/5">
             <span className="text-5xl font-bold">Marc Weber</span>
             <br></br>
-            <span className="text-2xl"> Full Stack Software Engineer </span>
+            <span className="text-2xl">Full Stack Software Engineer</span>
 
           </div>
           <div className="w-full">
+          <Card 
+            Date={card1.Date} 
+            Role={card1.Role} 
+            Description={card1.Description} 
+            Skills={card1.Skills} 
+          />
             About
             <br></br>
             work experience
