8d84fa945ca243f8f79535b13f5d5a44bf8b8e2e
MarcWeberFS <marc.robin.weber@gmail.com>
2024-07-05 05:53:17 +0200

    added framer motion and adjusted the hover for the cards.

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SkillBadge from './Skillbadge';
+import { motion } from 'framer-motion';
 import '../App.css';
 
 export default function Card({ Date, Role, Description, Skills, Link }) {
-    return (
-      <a href={Link} className="block cursor-pointer">
-        <div className="flex divide-gray-200 overflow-hidden px-4 py-4 rounded-lg bg-transparent hover-bg-transparent-50">
-          <div className="w-48 text-sm text-slate-400 italic ">
-            {Date}
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <a href={Link} className="block cursor-pointer">
+      <div
+        className="flex divide-gray-200 overflow-hidden px-4 py-4 rounded-lg bg-transparent hover-bg-transparent-50"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <div className="w-48 text-sm text-slate-400 italic">
+          {Date}
+        </div>
+        <div className="w-full">
+          <div className="flex items-center justify-between px-2 sm:px-3 text-xl font-semibold">
+            {Role}
+            <motion.span
+              className="material-icons ml-2 mb-auto"
+              animate={{
+                scale: isHovered ? 1.2 : 1,
+                x: isHovered ? 4 : 0,
+                y: isHovered ? -4 : 0
+              }}
+              transition={{ type: 'spring', stiffness: 300 }}
+            >
+              open_in_new
+            </motion.span>
           </div>
-          <div className="w-full">
-            <div className="flex items-center justify-between px-2 sm:px-3 text-xl font-semibold">
-              {Role}
-              <span className="material-icons ml-2">open_in_new</span>
-            </div>
-            <div className="px-2 py-2 sm:p-3 text-slate-300 font-normal">
-              {Description}
-            </div>
-            <div className="px-2">
-              <div className="flex flex-wrap">
-                {Skills.map((skill, index) => (
-                  <SkillBadge key={index} skill={skill} />
-                ))}
-              </div>
+          <div className="px-2 py-2 sm:p-3 text-slate-300 font-normal">
+            {Description}
+          </div>
+          <div className="px-2">
+            <div className="flex flex-wrap">
+              {Skills.map((skill, index) => (
+                <SkillBadge key={index} skill={skill} />
+              ))}
             </div>
           </div>
         </div>
-      </a>
-    );
-  }
+      </div>
+    </a>
+  );
+}
