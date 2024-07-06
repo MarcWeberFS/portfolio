cbcc144dce9687c084d9e3baf5de07a83190f849
MarcWeberFS <marc.robin.weber@gmail.com>
2024-07-06 04:49:53 +0200

    adjusted the styling of the button added a hover effect with framer motion

--- src/component/Download.js
+++ src/component/Download.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 export default function Download({ Label, Link }) {
     return (
-        <a 
-            className="bg-slate-900 text-white px-4 py-2 rounded-lg inline-flex items-center" 
+        <motion.a 
+            whileHover={{ scale: 1.1, backgroundColor: "#172554", borderColor: "#6366f1", color: "#ffffff" }}
+            className="px-4 py-2 backdrop-blur-sm border bg-blue-300/10 border-blue-500/20 text-white mx-auto text-center rounded-full relative mt-4" 
             href={Link} 
             download
         >
@@ -11,8 +13,6 @@ export default function Download({ Label, Link }) {
             <span className="material-icons ml-2 mb-auto text-lg">
                 download
             </span>
-        </a>
+        </motion.a>
     );
 }
-
-
