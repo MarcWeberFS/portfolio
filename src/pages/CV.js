525362168fde487ef12faa64a2a882fbd4a27c26
MarcWeberFS <marc.robin.weber@gmail.com>
2024-07-07 04:30:17 +0200

    styling mobile CV page

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import CV from '../assets/CV_Marc_Weber.pdf';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import '../App.css';
 
 // Documentation: https://react-pdf-viewer.dev/docs/basic-usage/
 
 const CVPage = () => {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-        <Viewer fileUrl={CV} />
-    </Worker>
-    );
-   };
+    <div className="pdf-container">
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <Viewer fileUrl={CV} plugins={[defaultLayoutPluginInstance]} />
+      </Worker>
+    </div>
+  );
+};
 
 export default CVPage;
