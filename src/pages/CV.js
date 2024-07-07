0f6fac71c10027569e28cec72e45599c44aab832
MarcWeberFS <marc.robin.weber@gmail.com>
2024-07-07 04:27:41 +0200

    fix pdf viewing page.

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import CV from '../assets/CV_Marc_Weber.pdf';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
+// Documentation: https://react-pdf-viewer.dev/docs/basic-usage/
 
 const CVPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${version}/build/pdf.worker.min.js`}>
-        <div style={{ height: '100vh', width: '100%', overflow: 'hidden' }}>
-          <Viewer fileUrl={CV} />
-        </div>
-      </Worker>
-    </div>
-  );
-};
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <Viewer fileUrl={CV} />
+    </Worker>
+    );
+   };
 
 export default CVPage;
